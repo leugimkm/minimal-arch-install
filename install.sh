0f93272c19ee780074546ae2003d09f19f93176a
leugimkm <leugim911@hotmail.com>
2022-09-18 23:06:36 -0500

    u: some fixes

--- install.sh
+++ install.sh
@@ -71,29 +71,39 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # --------------------------------------------- Change root into the new system
 arch-chroot /mnt /bin/bash <<EOF
 
+echo "Setting system clock..."
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
 
+echo "Setting locale..."
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 echo KEYMAP=$KEYMAP > /etc/vconsole.conf
 
+echo "Setting hostname..."
 echo $HOSTNAME > /etc/hostname
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 
-echo "Setting root password"
+echo "Setting root password..."
 echo -en "$ROOT_PASSWORD\n$ROOT_PASSWORD" | passwd
 
-echo "Creating new user"
+echo "Installing Sudo..."
+pacman -Sy
+pacman -S sudo
+
+echo "Creating new user..."
 useradd -m $USER_NAME
 useradd -aG wheel,audio,video,optical,storage $USER_NAME
 echo -en "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_PASSWORD
 echo "%wheel ALL=(ALL) ALL" | EDITOR="tee -a" visudo
 
+echo "Installing bootloader..."
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "Enabling NetworkManager..."
 pacman -S networkmanager
 systemctl enable NetworkManager
 EOF
