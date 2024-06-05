9808652e9906c7f7d28db37bc221ea23d01cf8bb
leugimkm <leugim911@hotmail.com>
2024-06-04 21:15:57 -0500

    u: todo

--- post-install.sh
+++ post-install.sh
@@ -9,7 +9,7 @@
 DOTFILES_DIR="$HOME/dotfiles"
 
 yes | sudo pacman -Syu
-yes | sudo pacman -S adobe-source-code-pro-fonts ttf-dejavu python-setuptools python-pip xorg-server xorg-xinit qtile kitty picom
+yes | sudo pacman -S adobe-source-code-pro-fonts ttf-dejavu python-setuptools python-pip python-pillow xorg-server xorg-xinit qtile kitty picom wget tree alsa-utils ranger
 
 git clone https://github.com/leugimkm/dotfiles "$DOTFILES_DIR"
 
@@ -26,5 +26,10 @@ done
 cp "$DOTFILES_DIR/.bashrc" "$HOME/"
 cp "$DOTFILES_DIR/.vimrc" "$HOME/"
 cp "$DOTFILES_DIR/.xinitrc" "$HOME/"
+
+# PYTHON_VERSION=$(python --version | awk '{print $2}' | cut -d. -f1-2)
+# POWERLINE_DIR="/usr/lib/python$PYTHON_VERSION/site-packages/powerline/config_files"
+# mkdir -p "$HOME/.config/powerline"
+# cp -rf "$POWERLINE_DIR" "$HOME/.config/powerline"
 
 echo "Done!"
