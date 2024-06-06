824d291f347bd8b6a22dc5f05b3d0874d6a47e29
leugimkm <leugim911@hotmail.com>
2024-06-06 11:12:18 -0500

    u: add colors

--- post-install.sh
+++ post-install.sh
@@ -6,6 +6,17 @@
 
 ###############################################################################
 
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+RESET=$'\e[0m'
+COLS=$(tput cols)
+
 print_info() {
   printf -- "${WHITE}=%.0s" $(seq 0 $(($COLS - (${#1} + 4))))
   echo "${GREEN} ${1}${RESET}"
