df108454709b795a356495dea8fbf93630b91cae
leugimkm <leugim911@hotmail.com>
2025-05-25 00:36:26 -0500

    update

--- install.sh
+++ install.sh
@@ -275,7 +275,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 if [ "$boot_loader" = "UEFI" ]; then
   grub_install_CMD="grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=GRUB --recheck"
 else
-  grub_install_CMD="grub-install --target=i386pc /dev/sda --recheck"
+  grub_install_CMD="grub-install --target=i386-pc /dev/sda"
 fi
 
 arch-chroot /mnt /bin/bash <<EOF
